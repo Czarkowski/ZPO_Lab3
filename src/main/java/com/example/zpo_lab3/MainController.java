cbbc95fa801ef67e20984beab0b44d169608930f
Czarkowski <cezarytytko@gmail.com>
2023-03-28 19:35:45 +0200

    28.03.2023

--- src/main/java/com/example/zpo_lab3/MainController.java
+++ src/main/java/com/example/zpo_lab3/MainController.java
@@ -4,7 +4,39 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+
 public class MainController {
+
+    private Socket socket = null;
+    private OutputStream output = null;
+    @FXML
+    private void initialize(){
+        String hostname = "localhost";
+        int port = 4447;
+
+        try{
+            socket = new Socket(hostname, port);
+            output = socket.getOutputStream();
+
+            output.write("Client online".getBytes(StandardCharsets.UTF_8));
+
+
+        } catch (UnknownHostException ex) {
+
+            System.out.println("Server not found: " + ex.getMessage());
+
+        } catch (IOException ex) {
+
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+    }
 
     @FXML
     private TextField NickText;
